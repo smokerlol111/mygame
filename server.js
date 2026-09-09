763715ffba81bbcc75bd6e3354a09ef03432d902
smokerlol111 <smokerlol1007@gmail.com>
2026-09-09 20:25:46 +0300

    v1.5.7 - replace Home Alone round 2 200 question

--- server.js
+++ server.js
@@ -776,7 +776,7 @@ io.on('connection', socket => {
   });
 });
 
-storage.init().then(()=>server.listen(PORT,'0.0.0.0',()=>console.log(`SMOKERLOL v1.5.6: http://0.0.0.0:${PORT}`))).catch(err=>{console.error('Storage init failed:',err);process.exit(1)});
+storage.init().then(()=>server.listen(PORT,'0.0.0.0',()=>console.log(`SMOKERLOL v1.5.7: http://0.0.0.0:${PORT}`))).catch(err=>{console.error('Storage init failed:',err);process.exit(1)});
 
 function shutdown(signal) {
   console.log(`${signal}: завершуємо роботу сервера...`);
